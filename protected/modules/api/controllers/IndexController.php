e9bb93739c928ad3f8eba869e86da7cc6fafa187
zhoujiafei <zhoujiafei@RD-Stephen-Chow.local>
2014-07-02 10:13:54 +0800

    首页要显示的数据
    首页要显示的数据

--- protected/modules/api/controllers/IndexController.php
+++ protected/modules/api/controllers/IndexController.php
@@ -1,9 +1,18 @@
 <?php
-
+//首页的界面
 class IndexController extends ApiController
 {
 	public function actionIndex()
 	{
-		$this->errorOutput(array('return' => 1));
+		//获取商家信息
+		$model = Shops::model()->with('image')->findAll('t.status=:status',array(':status' => 2));
+		$shopData = array();
+		foreach($model AS $k => $v)
+		{
+			$shopData[$k] = $v->attributes;
+			$shopData[$k]['logo'] = $shopData[$k]['logo']?Yii::app()->params['img_url'] . $v->image->filepath . $v->image->filename:'';
+		}
+		
+		print_r($shopData);
 	}
 }
