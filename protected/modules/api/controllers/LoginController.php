a226e46f723423de7cc7f3ca689b131f1ebb3bc2
zhoujiafei <zhoujiafei@RD-Stephen-Chow.local>
2014-07-02 17:54:59 +0800

    登陆接口
    登陆接口

--- protected/modules/api/controllers/LoginController.php
+++ protected/modules/api/controllers/LoginController.php
@@ -18,6 +18,22 @@ class LoginController extends ApiController
 			Error::output(Error::ERR_NO_PASSWORD);
 		}
 		
+		//获取用户模型
+		$userinfo = Members::model()->find('name=:name',array(':name' => $name));
+		if(!$userinfo)
+		{
+			Error::output(Error::ERR_NO_USER);
+		}
+		else 
+		{
+			$_password = md5($userinfo->salt . $password);
+			if($_password != $userinfo->password)
+			{
+				Error::output(Error::ERR_INVALID_PASSWORD);
+			}
+		}
+		
+		//登陆成功生成user_login
 		
 		
 		
@@ -25,15 +41,9 @@ class LoginController extends ApiController
 		
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		//登陆成功后返回用户数据
+		$member = CJSON::decode(CJSON::encode($userinfo));
+		unset($member['password'],$member['salt']);
+		echo json_encode($member);
 	}
 }
--- protected/modules/api/extensions/Error.php
+++ protected/modules/api/extensions/Error.php
@@ -6,6 +6,8 @@ class Error
 	const ERR_NO_LOGIN = 40002;
 	const ERR_NO_USER_NAME = 40003;
 	const ERR_NO_PASSWORD = 40004;
+	const ERR_NO_USER = 40005;
+	const ERR_INVALID_PASSWORD = 40006;
 	
 	public static function output($errorCode = '')
 	{
@@ -28,11 +30,13 @@ class Error
 	public static function getErrorInfo()
 	{
 		return array(
-			self::ERR_UNKNOW => '未知错误',
-			self::ERR_NO_ACCESS_TOKEN => '没有access_token',
-			self::ERR_NO_LOGIN => '未登录',
-			self::ERR_NO_USER_NAME => '用户名不能为空',
-			self::ERR_NO_PASSWORD => '密码不能为空',
+			self::ERR_UNKNOW 			=> '未知错误',
+			self::ERR_NO_ACCESS_TOKEN 	=> '没有access_token',
+			self::ERR_NO_LOGIN 			=> '未登录',
+			self::ERR_NO_USER_NAME 		=> '用户名不能为空',
+			self::ERR_NO_PASSWORD 		=> '密码不能为空',
+			self::ERR_NO_USER 			=> '用户不存在',
+			self::ERR_INVALID_PASSWORD 	=> '密码不合法',
 		);
 	}
 }
