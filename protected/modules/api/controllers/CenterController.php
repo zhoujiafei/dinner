4c6ebf5b3f159b89bd0f299838e9cb7790177531
zhoujiafei <564751169@qq.com>
2014-07-04 09:31:59 +0800

    完成修改密码接口
    完成修改密码接口

--- protected/modules/api/controllers/CenterController.php
+++ protected/modules/api/controllers/CenterController.php
@@ -5,7 +5,88 @@ class CenterController extends ApiController
 {
 	public function actionIndex()
 	{
-		$user = $this->module->user;
-		Out::jsonOutput($user);//返回用户信息
+		Out::jsonOutput($this->module->user);//返回用户信息
 	}
+	
+	//修改密码
+	public function actionModifyPassword()
+	{
+		$cur_password = Yii::app()->request->getPost('cur_password');//当前密码
+		$new_password = Yii::app()->request->getPost('new_password');//新密码
+		$comfirm_password = Yii::app()->request->getPost('comfirm_password');//确认新密码
+		
+		if(!$cur_password)
+		{
+			Error::output(Error::ERR_NO_PASSWORD);
+		}
+
+		if(!$new_password || !$comfirm_password)
+		{
+			Error::output(Error::ERR_NO_PASSWORD);
+		}
+		else if(strlen($new_password) > 15 || strlen($comfirm_password) > 15)
+		{
+			Error::output(Error::ERR_PASSWORD_TOO_LONG);
+		}
+		else if($new_password !== $comfirm_password)
+		{
+			Error::output(Error::ERR_TWO_PASSWORD_NOT_SAME);
+		}
+		
+		//判断该用户是不是已经存在了
+		$_member = Members::model()->find('id=:id',array(':id' => $this->module->user['id']));
+		if(!$_member)
+		{
+			Error::output(Error::ERR_NO_USER);
+		}
+		else if(md5($_member->salt . $cur_password) != $_member->password)
+		{
+			Error::output(Error::ERR_INVALID_ORI_PASSWORD);
+		}
+		
+		//随机长生一个干扰码
+		$salt = Common::getGenerateSalt();
+		$_member->salt = $salt;
+		$_member->password = md5($salt . $new_password);
+		$_member->update_time = time();
+		if($_member->save())
+		{
+			Out::jsonOutput(array('return' => 1));//修改成功
+		}
+		else 
+		{
+			Error::output(Error::ERR_SAVE_FAIL);
+		}
+	}
+	
+	//查看今日订单
+	public function actionTodayOrder()
+	{
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	//查看历史订单
+	public function actionHistoryOrder()
+	{
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- protected/modules/api/extensions/Error.php
+++ protected/modules/api/extensions/Error.php
@@ -10,7 +10,11 @@ class Error
 	const ERR_INVALID_PASSWORD = 40006;
 	const ERR_NO_SHOPID = 40007;
 	const ERR_NO_SHOP = 40008;
-	
+	const ERR_TWO_PASSWORD_NOT_SAME = 40009;
+	const ERR_PASSWORD_TOO_LONG = 50001;
+	const ERR_INVALID_ORI_PASSWORD = 50002;
+	const ERR_SAVE_FAIL = 50003;
+
 	public static function output($errorCode = '')
 	{
 		$errorMsg = self::getErrorInfo();
@@ -32,15 +36,19 @@ class Error
 	public static function getErrorInfo()
 	{
 		return array(
-			self::ERR_UNKNOW 			=> '未知错误',
-			self::ERR_NO_ACCESS_TOKEN 	=> '没有access_token',
-			self::ERR_NO_LOGIN 			=> '未登录',
-			self::ERR_NO_USER_NAME 		=> '用户名不能为空',
-			self::ERR_NO_PASSWORD 		=> '密码不能为空',
-			self::ERR_NO_USER 			=> '用户不存在',
-			self::ERR_INVALID_PASSWORD 	=> '密码不合法',
-			self::ERR_NO_SHOPID 		=> '没有商店id',
-			self::ERR_NO_SHOP 			=> '商店不存在',
+			self::ERR_UNKNOW 				=> '未知错误',
+			self::ERR_NO_ACCESS_TOKEN 		=> '没有access_token',
+			self::ERR_NO_LOGIN 				=> '未登录',
+			self::ERR_NO_USER_NAME 			=> '用户名不能为空',
+			self::ERR_NO_PASSWORD 			=> '密码不能为空',
+			self::ERR_NO_USER 				=> '用户不存在',
+			self::ERR_INVALID_PASSWORD 		=> '密码不合法',
+			self::ERR_NO_SHOPID 			=> '没有商店id',
+			self::ERR_NO_SHOP 				=> '商店不存在',
+			self::ERR_TWO_PASSWORD_NOT_SAME => '两次密码不相同',
+			self::ERR_PASSWORD_TOO_LONG 	=> '设置的密码过长',
+			self::ERR_INVALID_ORI_PASSWORD 	=> '原密码不合法',
+			self::ERR_SAVE_FAIL 			=> '保存失败',
 		);
 	}
 }
